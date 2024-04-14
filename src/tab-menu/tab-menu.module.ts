f4b19373781054089fbc32f5269e7488eaee4e94
Duong <duong.nh@open-dev.io>
2024-04-14 19:25:28 +0700

    tab menu

--- src/tab-menu/tab-menu.module.ts
+++ src/tab-menu/tab-menu.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TabMenuController } from './tab-menu.controller';
 import { TabMenuService } from './tab-menu.service';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CaslModule } from 'src/casl/casl.module';
 
 @Module({
+  imports: [CaslModule],
   controllers: [TabMenuController],
-  providers: [TabMenuService]
+  providers: [TabMenuService, JwtService, PrismaService]
 })
-export class TabMenuModule {}
+export class TabMenuModule { }
--- src/tab-menu/tab-menu.service.ts
+++ src/tab-menu/tab-menu.service.ts
@@ -1,4 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { TabMenuForPost } from './dto/TabMenuForPost';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { TabMenuForGet } from './dto/TabMenuForGet';
+import { TabMenuForUpdate } from './dto/TabMenuForUpdate';
 
 @Injectable()
-export class TabMenuService {}
+export class TabMenuService {
+  constructor(private readonly prismaService: PrismaService) { }
+  async createNewTabMenu(tabmenu: TabMenuForPost): Promise<TabMenuForGet> {
+    try {
+      return this.prismaService.tabMenu.create({
+        data: {
+          name: tabmenu.name,
+          iconUrl: tabmenu.iconUrl,
+          url: tabmenu.url,
+        }
+      })
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(error, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  async updateTabMenu(id: string, tabMenuForUpdate: TabMenuForUpdate): Promise<TabMenuForGet> {
+    try {
+      return this.prismaService.tabMenu.update({
+        where: { id },
+        data: {
+          name: tabMenuForUpdate.name,
+          iconUrl: tabMenuForUpdate.iconUrl,
+          url: tabMenuForUpdate.url
+        }
+      })
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(error, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  async getAllTabMenu(): Promise<Array<TabMenuForGet>> {
+    try {
+      return this.prismaService.tabMenu.findMany();
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(error, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  async deleteTabMenu(id: string): Promise<TabMenuForGet> {
+    try {
+      return this.prismaService.tabMenu.delete({
+        where: {
+          id
+        }
+      });
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(error, HttpStatus.BAD_REQUEST);
+    }
+  }
+}
